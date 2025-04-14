763326f334475ca6bf5ea7327e13b8e2e40760e1
FERNANDO DE OLIVEIRA ALMEIDA <fernandooar@gmail.com>
2025-04-13 21:32:20 -0300

    Refatoração geral: - Nova estrutura MVC - Separação de lógica JS (AJAX para exibição de senha) - Criptografia e descriptografia com AES-256 - Inclusão do campo login_servico na tabela servicos - Controle de sessão centralizado - Melhorias visuais com Bootstrap e Bootstrap Icons - Modal com exibição e cópia de senha

--- helpers/CryptoHelper.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function criptografarSenha($senha)
-{
-    $chave = 'sua_chave_super_secreta'; // Defina uma chave secreta
-    $iv = openssl_random_pseudo_bytes(16); // Gera um vetor de inicialização aleatório
-    $senha_criptografada =  openssl_encrypt($senha, 'AES-256-CBC', $chave, 0, $iv);
-    return base64_encode($senha_criptografada . '::' . $iv); // Retorna a senha criptografada e o vetor de inicialização separadas por :: para posterior descriptografia
-}
